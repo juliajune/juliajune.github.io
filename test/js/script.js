fa7b4329c32a6e65ec8a555d52cf81f1e550f4c0
juliajune <julia22@bk.ru>
2022-06-08 14:17:13 +0300

    edit test

--- test/js/script.js
+++ test/js/script.js
@@ -1,1 +1,10 @@
 console.log('Скрипт для всех браузеров');
+
+/* Scripts for IE11 only */
+var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+// true on IE11
+// false on Edge and other IEs/browsers. 
+console.log(isIE11);
+if (isIE11 == true){
+    console.log('Скрипт только для IE11');
+}
